024314397b584f26ee06012f5fe07c9823b0f850
Utilisateur <user@debian.arcahe.ovh>
2020-03-03 16:55:15 +0100

    modification

---
+++ src/main/java/uvsq21921358/LSP/App.java
@@ -0,0 +1,13 @@
+package uvsq21921358.LSP;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
